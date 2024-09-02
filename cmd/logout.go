f6d50c0618fabd4943be7dce44884d611e2f63c3
Heet-10104 <heetjhaveri61001@gmail.com>
2024-09-02 21:50:50 +0530

    user session maintained

--- cmd/logout.go
+++ cmd/logout.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -20,9 +22,68 @@ func init() {
 }
 
 func logoutFun(cmd *cobra.Command, args []string) {
-	err := os.Setenv("USER-ID", "")
+	envFile := ".env"
+
+	envMap, err := LoadEnvFile(envFile)
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
+
+	envMap["USER_ID"] = ""
+
+	err = WriteEnvFile(envFile, envMap)
+	if err != nil {
+		fmt.Println("Error writing to .env file:", err)
+	}
+
 	if err != nil {
 		fmt.Println("Error logging out")
 	}
 	fmt.Println("Logged out")
+}
+
+func LoadEnvFile(filename string) (map[string]string, error) {
+	envMap := make(map[string]string)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return envMap, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.TrimSpace(line) == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		keyValue := strings.SplitN(line, "=", 2)
+		if len(keyValue) == 2 {
+			envMap[strings.TrimSpace(keyValue[0])] = strings.TrimSpace(keyValue[1])
+		}
+	}
+
+	return envMap, scanner.Err()
+}
+
+func WriteEnvFile(filename string, envMap map[string]string) error {
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for key, value := range envMap {
+		_, err := file.WriteString(fmt.Sprintf("%s=%s\n", key, value))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
 	"s4/cmd"
+	"strings"
 
 	"github.com/joho/godotenv"
 
@@ -25,16 +27,14 @@ func loadDb() {
 
 func main() {
 
-	godotenv.Load(".env")
-	// if err != nil {
-	// 	log.Fatalf("Error loading .env file")
-	// 	fmt.Println(err)
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file", err)
+	}
 
 	loadDb()
 	var isSessionOn bool
-	var sessionUser = os.Getenv("USER-ID")
-	println(sessionUser)
+	var sessionUser = os.Getenv("USER_ID")
 	if sessionUser != "" {
 		isSessionOn = true
 	}
@@ -44,11 +44,10 @@ func main() {
 		fmt.Println("Enter username:")
 		fmt.Scanln(&username)
 
-		row := database.QueryRow("SELECT psw FROM users WHERE user=?", username)
+		row := database.QueryRow("SELECT psw FROM users WHERE username=?", username)
 
 		var q_psw string
 		err := row.Scan(&q_psw)
-
 		if err == sql.ErrNoRows {
 			fmt.Println("User not found")
 			fmt.Println("Register ? [Y/N]")
@@ -63,7 +62,7 @@ func main() {
 				fmt.Println("password:")
 				psw = newPassword()
 				fmt.Println(psw)
-				_, err := database.Exec("INSERT INTO users(user, psw) VALUES(?, ?)", username, psw)
+				_, err := database.Exec("INSERT INTO users(username, psw) VALUES(?, ?)", username, psw)
 				if err != nil {
 					log.Fatalln(err)
 				}
@@ -78,8 +77,23 @@ func main() {
 				return
 			}
 		}
-		os.Setenv("USER-ID", username)
-		x := os.Getenv("USER-ID")
+
+		envFile := ".env"
+
+		envMap, err := LoadEnvFile(envFile)
+		if err != nil {
+			fmt.Println("Error loading .env file:", err)
+			return
+		}
+
+		envMap["USER_ID"] = username
+
+		err = WriteEnvFile(envFile, envMap)
+		if err != nil {
+			fmt.Println("Error writing to .env file:", err)
+		}
+
+		x := os.Getenv("USER_ID")
 		fmt.Println("Welcome", x)
 	}
 
@@ -94,4 +108,49 @@ func newPassword() string {
 		password += string(charset[rand.Intn(len(charset))])
 	}
 	return password
+}
+
+func LoadEnvFile(filename string) (map[string]string, error) {
+	envMap := make(map[string]string)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return envMap, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.TrimSpace(line) == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		keyValue := strings.SplitN(line, "=", 2)
+		if len(keyValue) == 2 {
+			envMap[strings.TrimSpace(keyValue[0])] = strings.TrimSpace(keyValue[1])
+		}
+	}
+
+	return envMap, scanner.Err()
+}
+
+func WriteEnvFile(filename string, envMap map[string]string) error {
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for key, value := range envMap {
+		_, err := file.WriteString(fmt.Sprintf("%s=%s\n", key, value))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
